8e81c480797354dca3f66de7e598d01e771814ad
Jared G <jmattgiroux@gmail.com>
2020-12-09 00:33:11 -0600

    Set up files for program

---
+++ project/Main.java
@@ -0,0 +1,14 @@
+import pack.Game;
+import pack.Record;
+
+public class Main {
+
+    Game game;
+
+    Record record;
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+
+}
