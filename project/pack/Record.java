27212a0f7ec8c2aba39af0e1bc6cd64167f8b0b6
Jared G <jmattgiroux@gmail.com>
2020-12-09 01:53:21 -0600

    Finished stubbing functions, will now link them to set relationships between them. No actual code allowed before this step is done.

--- project/pack/Record.java
+++ project/pack/Record.java
@@ -1,8 +1,35 @@
 package pack;
 
+import java.util.ArrayList;
+
 public class Record {
 
-    // member variables
+    // Score nested class
+    public class Score {
+
+        // Score class member variables
+
+        String playerName;
+        Integer score;
+
+        // default constructor
+
+        public Score(){
+
+        }
+
+        // non-default constructor
+        // this constructor will be used for when we 
+        // parse the high scores file into Scores for
+        // our scoresRecord ArrayList
+        public Score(String playerName, Integer score)
+        {
+
+        }
+    }
+
+    // Record class member variables
+    ArrayList<Score> scoresRecord;
     
     // default constructor
     public Record()
@@ -11,4 +38,39 @@ public class Record {
     }
 
     // member methods
+
+    // reads strings from txt file and puts strings into
+    public void readFile(){
+
+    }
+
+    //clears old save file, basically does opposite process
+    // of readFile using a similar looping mechanism
+    public void writeFile(){
+
+    }
+
+    // for if no highscores file exists
+    public void createFile(){
+
+    }
+
+    // displays top 10 scores
+    public void displayScores(){
+
+    }
+
+
+    public void promptForName(){
+
+    }
+
+    // calls promptForName, adds a new Score element to our scoresRecord ArrayList
+    public void recordScore(){
+        //something like
+        // Score score = new Score(promptForName(), score);
+        //scoresRecord.add(score);
+
+    }
 }
+
